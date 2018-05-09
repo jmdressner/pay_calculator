b7562db972164af63d87e836c21582b51316a948
Julie Dressner <jmdressner@gmail.com>
2018-05-09 10:50:12 -0500

    updating project files

---
+++ app.js
@@ -0,0 +1,8 @@
+let hours = prompt("How many hours did you work this week");
+let payRate = prompt("What is your pay rate per hour");
+
+hours = Number(hours);
+payRate = Number(payRate);
+
+console.log(hours*payRate);
+
