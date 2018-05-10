f3e7de5d1eea4976a22a2730c332ade9f829a1de
Julie Dressner <jmdressner@gmail.com>
2018-05-10 07:13:51 -0500

    overtime pay update

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ let payRate = prompt("What is your pay rate per hour");
 hours = Number(hours);
 payRate = Number(payRate);
 
-console.log(hours*payRate);
+if (hours=>40) {
+	console.log((hours*payRate)+((hours-40)*(payRate*0.5)));
+}
+else {
+	console.log(hours*payRate);
+}
 
